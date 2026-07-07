97aa140dfb9556de5d4fbce1e2a46f6c8e3d9e1d
DilshaAppuHennadi <appudil636@gmail.com>
2026-07-07 14:59:34 -0400

    Updated WaveguideLoss script
    Updates to WaveguideLoss file include: changing the dat file names to new files, adding system loss compensation to measured data, plotting the transmission for each waveguide at 1550nm, and performing curve fitting to determine waveguide and insertion losses.

--- WaveguideLoss.m
+++ WaveguideLoss.m
@@ -7,41 +7,121 @@
 
 clear all
 close all
+clc
+
+% Set loss variables for TE and TM. The first column contains the lengths
+% of the three different wire waveguides (in mm). The second column is
+% initialized to zero (0) and eventually overwritten with the transmission
+% measured for each waveguide at 1550 nm (in dBm). The loss of the
+% measurement system (everything external to the chip) was reported to be
+% 1dB. The transmission values used to calculate waveguide loss will be
+% compensated for system loss.
+
+loss_TE = [3 0;9 0;21 0];
+loss_TM = [3 0;9 0;21 0];
 
 %% 3mm Waveguide (shortest)
 
-A = 'Khaled/bar1_loss_898um_TE.dat';
-name = '3mm TE v1';
-plotData(A, name)
+A = 'Jul_03_2026/WGs/short_1_890um/LOSS_SHORT_1_TE.dat';
+name = 'SHORT TE';
+loss_TE(1,2) = plotData(A, name);
 
-B = 'Khaled/bar1_loss_898um_TM.dat';
-name = '3mm TM v1';
-plotData(B, name)
+B = 'Jul_03_2026/WGs/short_1_890um/LOSS_SHORT_1_TM.dat';
+name = 'SHORT TM';
+loss_TM(1,2) = plotData(B, name);
 
-C = 'Khaled/bar1_2nd_loss_898um_TE.dat';
-name = '3mm TE v2';
-plotData(C, name)
+%% 3mm Waveguide (medium)
 
-D = 'Khaled/bar1_2nd_loss_898um_TM.dat';
-name = '3mm TM v2';
-plotData(D, name)
+C = 'Jul_03_2026/WGs/med_1_2776um/LOSS_MED_1_TE.dat';
+name = 'MED TE';
+loss_TE(2,2) = plotData(C, name);
 
+D = 'Jul_03_2026/WGs/med_1_2776um/LOSS_MED_1_TM.dat';
+name = 'MED TM';
+loss_TM(2,2) = plotData(D, name);
 
-function plotData(filename, name)
+%% 3mm Waveguide (longest)
+
+E = 'Jul_03_2026/WGs/long_1_6499um/LOSS_LONG_1_TE.dat';
+name = 'LONG TE';
+loss_TE(3,2) = plotData(E, name);
+
+F = 'Jul_03_2026/WGs/long_1_6499um/LOSS_LONG_1_TM.dat';
+name = 'LONG TM';
+loss_TM(3,2) = plotData(F, name);
+
+%% Plot Transmission by waveguide length
+% We perform a polyfit() of the loss data for the three waveguides at
+% 1550nm. The polyfit() function is set to n=1 to obtain a linear fit. In
+% this case p_1 is the slope which will be in units of dBm/mm - this is
+% considered to be the waveguide loss. The y-intercept is the theoretical
+% loss at waveguide length = 0mm. This is used to represent the loss
+% resulting from coupling in about out of the chips via the edge couplers -
+% labelled as "insertion loss".
+
+p_TE = polyfit(loss_TE(:,1), loss_TE(:,2),1);
+p_TM = polyfit(loss_TM(:,1), loss_TM(:,2),1);
+
+dummy_lengths = linspace(0,25,25); %mm
+lin_fit_TE = polyval(p_TE, dummy_lengths);
+lin_fit_TM = polyval(p_TM, dummy_lengths);
+
+subplot(2,1,1);
+plot(loss_TE(:,1), loss_TE(:,2),'o')
+hold on
+plot(dummy_lengths,lin_fit_TE)
+hold off
+legend('Measured','Fitted')
+xlabel('Waveguide length (mm)')
+ylabel('Power (dBm)')
+title('TE')
+
+subplot(2,1,2);
+plot(loss_TM(:,1), loss_TM(:,2),'o')
+hold on
+plot(dummy_lengths,lin_fit_TM)
+hold off
+legend('Measured','Fitted')
+xlabel('Waveguide length (mm)')
+ylabel('Power (dBm)')
+title('TM')
+
+sgtitle('Transmission (1550nm) variation with waveguide length')
+
+fprintf('Waveguide loss for TE is: %f dBm/mm.\n', p_TE(1));
+fprintf('Waveguide loss for TM is: %f dBm/mm.\n', p_TM(1));
+fprintf('Insertion loss of both edge couplers for TE is: %f dBm.\n', p_TE(2));
+fprintf('Insertion loss of both edge couplers for TM is: %f dBm.\n', p_TM(2));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function lossdB = plotData(filename, name)
     A = importdata(filename);
     lambda_nm = A(:,1);
     power = A(:,2); % mW
     powerdB = 10*log10(power); % dBm
+    powerdB_comp = powerdB - (-1);
 
     % Plot wavelength sweep in dBm
     figure
+    subplot(2,1,1);
     plot(lambda_nm, powerdB)
     xlabel('Wavelength (nm)')
     ylabel('Measured Power (dBm)')
     title(name)
 
+    subplot(2,1,2);
+    plot(lambda_nm, powerdB_comp)
+    xlabel('Wavelength (nm)')
+    ylabel('Measured Power (dBm)')
+    title(strcat(name, ' with system loss compensation'))
+
     % Pull transmission at 1550nm
     % later version of script: return this transmission value
     T_1550 = powerdB(find(lambda_nm == 1550));
     fprintf('The power for %s at 1550nm is: %f.\n', name, T_1550);
+    T_1550_comp = powerdB_comp(find(lambda_nm == 1550));
+    fprintf('Compensating system loss, the power for %s at 1550nm is: %f.\n', name, T_1550_comp);
+
+    lossdB = T_1550_comp;
 end
